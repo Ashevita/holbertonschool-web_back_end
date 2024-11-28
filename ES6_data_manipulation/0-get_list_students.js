909891191337c66e7a9fc14c9d265ba6c4ae7394
Ashevita <9362@holbertonstudents.com>
2024-11-28 15:18:19 +0100

    Data Manip

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,14 +1,7 @@
 export default function getListStudents() {
-
-  const listStudents = [
+  return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
-
-  return listStudents.map((student) => ({
-    id: student.id,
-    firstName: student.firstName,
-    location: student.location,
-  }));
 }
