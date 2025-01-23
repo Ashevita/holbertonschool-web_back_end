5921cf3ff837f8aada62db92de3547fb8966a6bc
Ashevita <9362@holbertonstudents.com>
2025-01-23 15:46:13 +0100

    correction

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
-#!/usr/bin/env node
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
 
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data.trim()}`);
+  if (data) {
+    process.stdout.write(`Your name is: ${data}`);
+  }
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
