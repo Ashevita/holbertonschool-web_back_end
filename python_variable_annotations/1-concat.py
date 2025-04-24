4169c7f5bf5aa678991910de96ee4f082b997739
Ashevita <henri.53@hotmail.fr>
2025-04-24 10:03:10 +0200

    Delete python_variable_annotations directory

--- python_variable_annotations/1-concat.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-""" module contaning concat function typed """
-
-
-def concat(str1: str, str2: str) -> str:
-    """ function concatenating two string """
-    return str1 + str2
