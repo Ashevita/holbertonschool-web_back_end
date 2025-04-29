3fd6772cba7484b6f122f549a816f8cd3cc36ac3
Ashevita <henri@AshePC>
2025-04-29 10:28:04 +0200

    correction

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,9 +4,11 @@ import asyncio
 import random
 import typing
 
+
 async def async_generator(): -> typing.Generator[float, None, None]:
     """Coroutine qui génère 10 nombres aléatoires entre 0 et 10,
     avec une pause d'une seconde entre chaque génération."""
-    for _ in range(10):
+
+    for _ in range(0, 10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
